a68b97dc9df6e71c563c97f9353604f424c166ca
rlcarneiro97 <rafaeldelima97@hotmail.com>
2021-02-27 23:40:40 -0300

    adicionando outros modulos

---
+++ musica/src/main/java/br/com/emmanuelneri/MusicaApplication.java
@@ -0,0 +1,13 @@
+package br.com.emmanuelneri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicaApplication.class, args);
+	}
+
+}
